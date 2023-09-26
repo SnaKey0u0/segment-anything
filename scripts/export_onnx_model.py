1e17cc4182d7845b6409dfffc82a7b3d65b92625
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2023-09-26 12:27:58 +0800

    start

--- scripts/export_onnx_model.py
+++ scripts/export_onnx_model.py
@@ -6,8 +6,8 @@
 
 import torch
 
-from segment_anything import sam_model_registry
-from segment_anything.utils.onnx import SamOnnxModel
+from sam import sam_model_registry
+from sam.utils.onnx import SamOnnxModel
 
 import argparse
 import warnings
